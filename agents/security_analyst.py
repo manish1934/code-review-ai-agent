60f8fe8641fc18a5fa647c047742a4ac69af26fa
Manish Chauhan <manish.chauhaan44@gmail.com>
2026-05-10 10:29:12 +0530

    Update security_analyst.py

--- agents/security_analyst.py
+++ agents/security_analyst.py
@@ -1,5 +1,5 @@
 from langchain_groq import ChatGroq
-from langchain.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 import os
 from dotenv import load_dotenv
 
